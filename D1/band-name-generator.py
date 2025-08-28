f6c281ac194b129f6431225ca43bfa3caae0bb8a
pmigoku <pmigoku0@gmail.com>
2025-08-28 18:45:20 -0400

    Initian commit, day 1 complete

---
+++ D1/band-name-generator.py
@@ -0,0 +1,7 @@
+# Exercise 1 - Band Name Generator
+print("Welcome to the band name generator")
+city = input("What is the name of the city you grew up in?")
+print(city)
+pet = input("What is your pet's name?")
+print(pet)
+print("Your band name could be " + city + " " + pet)
